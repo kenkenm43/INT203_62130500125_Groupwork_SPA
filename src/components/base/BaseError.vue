cb4b0b3cf9e9e3817d2a93aeeb743b9aacee6da5
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-15 16:28:09 +0700

    '62130500125FeatureComponentGlobalRegistry[DisplayErrorOfInput]

---
+++ src/components/base/BaseError.vue
@@ -0,0 +1,39 @@
+<template
+  ><div>
+    <div v-for="(er, name) in this.errors" :key="er">
+      <div v-if="name.toUpperCase() == field.toUpperCase()">
+        <div v-if="er === '' ? true : false" class="text-red-600">
+          **** Please Enter Your {{ capitalize(field) }} ****
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  props: {
+    errors: {
+      type: Object,
+      require: true,
+    },
+    field: {
+      type: String,
+      require: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    capitalize(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
+};
+</script>
+
+<style></style>
