8ed150b61bcb770b0023185fd4f4809357b3759e
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-15 16:35:22 +0700

    62130500125ComponentNavBarAndRouterlink

---
+++ src/components/layout/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav">
+    <div class="text-white">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/form">Form</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.nav {
+  background-color: black;
+}
+a:link {
+  text-decoration: none;
+}
+</style>
