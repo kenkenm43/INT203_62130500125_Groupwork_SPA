f073b28155c7ea9d1b8204c6a86a000ce9d0b1c8
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-15 16:21:41 +0700

    '62130500125Feature[Component]DisplayError

---
+++ src/components/base/BaseView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div
+      class="border border-gray-50 rounded-xl shadow p-2 my-1 container bg-green-50"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
