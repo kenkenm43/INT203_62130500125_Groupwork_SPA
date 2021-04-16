72ce12be615dd5981d774387cb1e0581915ae141
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-16 13:32:42 +0700

    62130500125ComponentButton

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button
+      class="border-2 border-black bg-green-600 text-white p-2 rounded-md"
+    >
+      {{ msg }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style></style>
