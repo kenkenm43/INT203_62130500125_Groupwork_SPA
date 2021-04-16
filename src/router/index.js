b4f05b6866b86814456c1941775397afc289daf2
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-16 18:51:59 +0700

    62130500125router

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
 import Form from '@/views/Form.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+    path: '/info',
+    name: 'info',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Info.vue')
   }
   ,
   {
-    path: '/form',
+    path: '/',
     name: 'Form',
     component: Form
   }
