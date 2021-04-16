5285787aac8d574bc977716f18c5fa5ad183d907
kenkenm43 <nattawat.2543@mail.kmutt.ac.th>
2021-04-16 13:16:57 +0700

    62130500125GlobalComponent

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import BaseView from './components/base/BaseView.vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import BaseError from '@/components/base/BaseError.vue'
+const app  = createApp(App)
+app.component('base-view',BaseView)
+app.component('base-button',BaseButton)
+app.component('base-error',BaseError)
+app.use(router)
+app.mount('#app')
